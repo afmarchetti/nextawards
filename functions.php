501a98dd2c6a863c5a710276d4c751550b89f922
Andrea Marchetti <afmarchetti@users.noreply.github.com>
2025-04-23 11:47:38 +0200

    add cf7 button submit stlye

--- functions.php
+++ functions.php
@@ -224,7 +224,7 @@ function nextawards_customize_css(){
 	echo ':root { --site-bg: #'.$nextawards_bg_color.'; --link-color: '.esc_attr(get_theme_mod( 'nextawards_link_color', '#048ea0')).'; --link-color-hover: '.esc_attr(get_theme_mod( 'nextawards_link_color_hover', '#105862')).'; }';
 	echo 'body, :root :where(body), p, ul, li, ol{font-family: '.$nextawards_google_font_body.'}';
 	echo 'h1,h2,h3,h4,h5,h6{font-family: '.$nextawards_google_font.'}';
-	echo '.wp-block-button__link{background-color: '.esc_attr(get_theme_mod( 'nextawards_link_color', '#048ea0')).'}';
+	echo '.wp-block-button__link, input[type=submit].wpcf7-submit{background-color: '.esc_attr(get_theme_mod( 'nextawards_link_color', '#048ea0')).'}';
     echo '.wp-block-button__link:hover:not(.is-style-outline .wp-block-button__link){background-color: '.esc_attr(get_theme_mod( 'nextawards_link_color_hover', '#105862')).'}';
 	echo '.is-style-outline .wp-block-button__link:hover{color: '. esc_attr(get_theme_mod( 'nextawards_link_color_hover', '#105862')).'}';
 	echo '.header {background-color: '.esc_attr(get_theme_mod( 'nextawards_header_color', '#E4E4E4')).'}';
--- readme.txt
+++ readme.txt
@@ -53,6 +53,13 @@ TODO:
 - check block variation quote and create one for buttons
 - test spacing fuild system like oll. theme
 
+== 1.6.6 ==
+* add css for accessibility
+* add roles for accessibiliyt
+* fix search forms form for accessibility
+* fix alt logos for accesibility
+* Add cf7 button submit style
+
 == 1.6.5 ==
 * fix font-weight bold on strong 
 
